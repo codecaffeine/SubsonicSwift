63cd977b4b9ecfb9477e6437a813c564cde52647
Matt Thomas <matt@codecaffeine.com>
2014-06-03 21:33:46 -0400

    Initial Commit

---
+++ SubsonicSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SubsonicSwift
+//
+//  Created by Matt Thomas on 6/3/14.
+//  Copyright (c) 2014 Matt Thomas. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SubsonicSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SubsonicSwift
+//
+//  Created by Matt Thomas on 6/3/14.
+//  Copyright (c) 2014 Matt Thomas. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
