bb09965278bcc9ad5ce890c313d35e70cfc59da0
Matt Thomas <matt@codecaffeine.com>
2014-06-15 16:45:00 -0400

    Add LoginViewController

--- SubsonicSwift/LoginViewController.swift
+++ SubsonicSwift/LoginViewController.swift
@@ -10,9 +10,20 @@ import Cocoa
 
 class LoginViewController: NSViewController {
 
-    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        // Initialization code here.
+    @IBOutlet var serverField : NSTextField
+    @IBOutlet var userField : NSTextField
+    @IBOutlet var passwordField : NSSecureTextField
+    @IBOutlet var signInButton : NSButton
+
+    let signInAction: ((server: String, user: String, password: String) -> Void)
+
+
+    init(signInAction: ((server: String, user: String, password: String) -> Void)) {
+        self.signInAction = signInAction
+        super.init(nibName: "LoginViewController", bundle: nil)
     }
-    
+
+    @IBAction func signInPressed(sender : AnyObject) {
+        signInAction(server: serverField.stringValue, user: userField.stringValue, password: passwordField.stringValue)
+    }
 }
