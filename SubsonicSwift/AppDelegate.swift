6a8eef7cdf20279cfd6947bb4fb1e850b1577cba
Matt Thomas <matt@codecaffeine.com>
2014-06-15 16:45:12 -0400

    use authentication & login controller

--- SubsonicSwift/AppDelegate.swift
+++ SubsonicSwift/AppDelegate.swift
@@ -8,39 +8,83 @@
 
 import Cocoa
 
+let ProtectionSpace = "ProtectionSpace"
+let AppName = "Subsonic Swift"
+
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
-    @IBOutlet var serverField : NSTextField
-    @IBOutlet var usernameField : NSTextField
-    @IBOutlet var passwordField : NSSecureTextField
     @IBOutlet var window: NSWindow
-    var client: SubsonicClient?
+    let client: SubsonicClient
+    var loginViewController: LoginViewController?
+    let loginProtectionSpace: NSURLProtectionSpace?
+
+    init() {
+        client = SubsonicClient(appName: AppName,
+            sessionConfig: NSURLSessionConfiguration.defaultSessionConfiguration())
+        super.init()
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
+
+        if let data = NSUserDefaults.standardUserDefaults().objectForKey(ProtectionSpace) as? NSData {
+            if let space = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? NSURLProtectionSpace {
+                if let credential = NSURLCredentialStorage.sharedCredentialStorage().credentialsForProtectionSpace(space).objectEnumerator().nextObject() as? NSURLCredential {
+                    let url = NSURLComponents()
+                    url.host = space.host;
+                    url.scheme = space.`protocol`
+
+                    self.client.authenticate(baseURL: url.URL.absoluteString, user: credential.user, password: credential.password) {
+                        (responseCredential, response, error) in
+
+                        println("from keychain: \(responseCredential)")
+                        if (!responseCredential) {
+                            NSURLCredentialStorage.sharedCredentialStorage().removeCredential(credential, forProtectionSpace: space)
+                        }
+                    }
+                }
+            }
+        }
+
+        let authenticationCallback = { [weak self]
+            (credential: NSURLCredential!, response: NSURLResponse!,
+            error: NSError!) -> Void in
+
+            println("from login box: \(credential)")
+            if credential {
+                let protectionSpace = NSURLProtectionSpace(host: self?.client.urlComponents?.host, port: 0, `protocol`: self?.client.urlComponents?.scheme, realm: nil, authenticationMethod: NSURLAuthenticationMethodHTTPBasic)
+
+                NSURLCredentialStorage.sharedCredentialStorage().setCredential(credential, forProtectionSpace: protectionSpace)
+
+                let data = NSKeyedArchiver.archivedDataWithRootObject(protectionSpace)
+                if data {
+                    NSUserDefaults.standardUserDefaults().setObject(data!, forKey: ProtectionSpace)
+                }
+            }
+        }
+
+        loginViewController = LoginViewController() {
+            [weak self] (server, user, password) in
+
+            if self {
+                self!.client.authenticate(baseURL: server, user: user, password: password, completion: authenticationCallback)
+            }
+        }
+
+        if let viewController = loginViewController {
+            window.contentView.addSubview(viewController.view)
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
     }
 
-    @IBAction func pingPressed(sender : AnyObject) {
-        self.client = SubsonicClient(appName: "Matt’s Awesome App",
-            baseURL: serverField.stringValue,
-            sessionConfig: NSURLSessionConfiguration.defaultSessionConfiguration())
-
-        if let client = self.client {
-            client.authenticate(user: usernameField.stringValue,
-                password: passwordField.stringValue) { credential, response, error in
-                    println("credential: \(credential)\nresponse: \(response)\nerror: \(error)")
-            }
-        }
-    }
-    @IBAction func artistsPressed(sender : NSButton) {
-        if let client = self.client {
-            client.artists {
-                data, response, error in
-                NSLog("data: \(data)\nresponse: \(response)\nerror: \(error)")
-            }
-        }
-    }
+//    @IBAction func artistsPressed(sender : NSButton) {
+//        if let client = self.client {
+//            client.artists {
+//                data, response, error in
+//                NSLog("data: \(data)\nresponse: \(response)\nerror: \(error)")
+//            }
+//        }
+//    }
 }
 
