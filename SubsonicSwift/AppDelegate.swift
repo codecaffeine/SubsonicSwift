049ebd4ef834eda2633262a4058809d8b5d39cb1
Matt Thomas <matt@codecaffeine.com>
2014-06-16 21:33:51 -0400

    Fixing authentication process

--- SubsonicSwift/AppDelegate.swift
+++ SubsonicSwift/AppDelegate.swift
@@ -24,7 +24,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         super.init()
     }
 
+    func saveNewCredentialToKeychain(credential: NSURLCredential) {
+        let protectionSpace = NSURLProtectionSpace(host: self.client.urlComponents?.host, port: 0, `protocol`: self.client.urlComponents?.scheme, realm: nil, authenticationMethod: NSURLAuthenticationMethodHTTPBasic)
+
+        NSURLCredentialStorage.sharedCredentialStorage().setCredential(credential, forProtectionSpace: protectionSpace)
+
+        let data = NSKeyedArchiver.archivedDataWithRootObject(protectionSpace)
+        if data {
+            NSUserDefaults.standardUserDefaults().setObject(data!, forKey: ProtectionSpace)
+        }
+    }
+
+    func removeOldCredentialFromKeychain() {
+        if let data = NSUserDefaults.standardUserDefaults().objectForKey(ProtectionSpace) as? NSData {
+            if let space = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? NSURLProtectionSpace {
+                if let credential = NSURLCredentialStorage.sharedCredentialStorage().credentialsForProtectionSpace(space).objectEnumerator().nextObject() as? NSURLCredential {
+                    NSURLCredentialStorage.sharedCredentialStorage().removeCredential(credential, forProtectionSpace: space)
+                }
+            }
+            NSUserDefaults.standardUserDefaults().removeObjectForKey(ProtectionSpace)
+        }
+    }
+
+    func loadArtists() {
+        self.client.artists {
+            data, reponse, error in
+
+            if data {
+                let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(), error: nil) as? Dictionary<String, AnyObject>
+                println("artists: \(jsonDict)")
+            } else {
+                println("error: \(error)")
+            }
+        }
+    }
+
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
+
+        let authenticationCallback = {
+            [weak self] (credential: NSURLCredential!, response: NSURLResponse!, error: NSError!) -> Void in
+
+            if credential {
+                self?.saveNewCredentialToKeychain(credential)
+                self?.loadArtists()
+            } else {
+                self?.removeOldCredentialFromKeychain()
+            }
+        }
 
         if let data = NSUserDefaults.standardUserDefaults().objectForKey(ProtectionSpace) as? NSData {
             if let space = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? NSURLProtectionSpace {
@@ -33,31 +79,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     url.host = space.host;
                     url.scheme = space.`protocol`
 
-                    self.client.authenticate(baseURL: url.URL.absoluteString, user: credential.user, password: credential.password) {
-                        (responseCredential, response, error) in
-
-                        println("from keychain: \(responseCredential)")
-                        if (!responseCredential) {
-                            NSURLCredentialStorage.sharedCredentialStorage().removeCredential(credential, forProtectionSpace: space)
-                        }
-                    }
-                }
-            }
-        }
-
-        let authenticationCallback = { [weak self]
-            (credential: NSURLCredential!, response: NSURLResponse!,
-            error: NSError!) -> Void in
-
-            println("from login box: \(credential)")
-            if credential {
-                let protectionSpace = NSURLProtectionSpace(host: self?.client.urlComponents?.host, port: 0, `protocol`: self?.client.urlComponents?.scheme, realm: nil, authenticationMethod: NSURLAuthenticationMethodHTTPBasic)
-
-                NSURLCredentialStorage.sharedCredentialStorage().setCredential(credential, forProtectionSpace: protectionSpace)
-
-                let data = NSKeyedArchiver.archivedDataWithRootObject(protectionSpace)
-                if data {
-                    NSUserDefaults.standardUserDefaults().setObject(data!, forKey: ProtectionSpace)
+                    self.client.authenticate(baseURL: url.URL.absoluteString, user: credential.user, password: credential.password, completion: authenticationCallback)
                 }
             }
         }
